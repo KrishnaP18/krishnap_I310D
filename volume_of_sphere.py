9bd0364e3e652fac98df6f180bcd5a157f44321e
Vishnu Pillai <krishnap0518@gmail.com>
2025-04-09 10:23:29 -0500

    created volume of a sphere function

--- volume_of_sphere.py
+++ volume_of_sphere.py
@@ -1,1 +1,7 @@
+def calculate_volume_of_sphere(radius):
+    constant = 4 / 3
+    pi = 3.14
+    return constant * pi * pow(radius, 3)
 
+print(int(calculate_volume_of_sphere(30)))
+print(int(calculate_volume_of_sphere(40)))
